0ce24621213c16da8a2a943496698e44f941c72c
Addison Schiller <acschiller@gmail.com>
2016-02-13 10:09:39 -0800

    Working on the transition, not super pretty atm

--- inventory.py
+++ inventory.py
@@ -94,7 +94,7 @@ class Inventory(object):
             self.borders.append(border)
             i += 1
 
-    def update(self):
+    def draw(self):
         if self.flag:
             self.update_inventory()
             InventoryBatch.draw()
