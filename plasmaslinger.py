df2d413f38014256d640833033fee811539427b6
alexschiller <alexschiller@gmail.com>
2016-03-08 20:42:25 -0500

    rework character ability count and choices (fewer choices atm)

--- plasmaslinger.py
+++ plasmaslinger.py
@@ -123,90 +123,11 @@ class PlasmaCore(Core):
             if not self.lightning_counter:
                 self.lightning = []
 
-# Unfinished
-class PSPDRepurpose(Skill):
-    def __init__(self, master, level, handler):
-        super(PSPDRepurpose, self).__init__(master, level, handler)
-        self.sound = load_sound('repurpose.wav')
-
-    def fire(self):
-        if self.handler.owner.stats.shield and self.handler.core.plasma != self.handler.core.plasma_max:
-            play_sound(self.sound)
-            self.master.spriteeffect.teleport(self.handler.owner.sprite.x, self.handler.owner.sprite.y)
-            self.handler.core.plasma += 50 * (self.handler.owner.stats.shield / self.handler.owner.stats.shield_max)
-            self.handler.owner.stats.update_health(self.handler.owner.stats.shield)
-            return True
-        return False
 
 # Unfinished
-class PSPDFlakJacket(Skill):
+class LDBreaker(Skill):
     def __init__(self, master, level, handler):
-        super(PSPDFlakJacket, self).__init__(master, level, handler)
-
-# Unfinished
-class PSPDTwentyMileMarch(Skill):
-    def __init__(self, master, level, handler):
-        super(PSPDTwentyMileMarch, self).__init__(master, level, handler)
-
-# Unfinished
-class PSPDLockAndLoad(Skill):
-    def __init__(self, master, level, handler):
-        super(PSPDLockAndLoad, self).__init__(master, level, handler)
-
-# Unfinished
-class PSPDFletcher(Skill):
-    def __init__(self, master, level, handler):
-        super(PSPDFletcher, self).__init__(master, level, handler)
-
-# Unfinished
-class PSPDSpotAndShot(Skill):
-    def __init__(self, master, level, handler):
-        super(PSPDSpotAndShot, self).__init__(master, level, handler)
-
-# Unfinished
-class PSPDIronRations(Skill):
-    def __init__(self, master, level, handler):
-        super(PSPDIronRations, self).__init__(master, level, handler)
-
-# Unfinished
-class PSPDStrafe(Skill):
-    def __init__(self, master, level, handler):
-        super(PSPDStrafe, self).__init__(master, level, handler)
-
-# Unfinished
-class PSPDCannibalize(Skill):
-    def __init__(self, master, level, handler):
-        super(PSPDCannibalize, self).__init__(master, level, handler)
-
-# Unfinished
-class PSPDBarrage(Skill):
-    def __init__(self, master, level, handler):
-        super(PSPDBarrage, self).__init__(master, level, handler)
-
-# Unfinished
-class PSMABang(Skill):
-    def __init__(self, master, level, handler):
-        super(PSMABang, self).__init__(master, level, handler)
-    #     self.image = load_image('psma.png')
-    #     self.damage_mod = 1 + .05 * self.level
-
-    # def fire(self):
-    #     if self.handler.core.bullets:
-    #         self.handler.core.bullets -= 1
-    #         gun = self.handler.copy_gun()
-    #         gun['damage_min'] = int(gun['damage_min'] * self.damage_mod)
-    #         gun['damage_max'] = int(gun['damage_max'] * self.damage_mod)
-    #         # gun['velocity'] = 30
-    #         gun['image'] = self.image
-
-    #         PlayerGunshot(self.master, self.handler, self, dict.copy(gun), self.handler.owner.sprite.x, self.handler.owner.sprite.y)
-    #         return True
-    #     return False
-
-# Unfinished
-class PSMAShieldSplitter(Skill):
-    def __init__(self, master, level, handler):
-        super(PSMAShieldSplitter, self).__init__(master, level, handler)
+        super(LDBreaker, self).__init__(master, level, handler)
         self.img = load_image('strike.png')
         self.sound = load_sound('slash.wav')
 
@@ -243,10 +164,11 @@ class PSMAShieldSplitter(Skill):
             return dist
         return False
 
+
 # Unfinished
-class PSMALightSword(Skill):
+class LDCurrent(Skill):
     def __init__(self, master, level, handler):
-        super(PSMALightSword, self).__init__(master, level, handler)
+        super(LDCurrent, self).__init__(master, level, handler)
         self.img = load_image('lightsword.png')
         self.sound = load_sound('slash.wav')
 
@@ -285,79 +207,9 @@ class PSMALightSword(Skill):
         return False
 
 # Unfinished
-class PSMALightSpeed(Skill):
+class LDZap(Skill):
     def __init__(self, master, level, handler):
-        super(PSMALightSpeed, self).__init__(master, level, handler)
-        self.img = load_image('strike.png')
-        self.sound = load_sound('slash.wav')
-
-    def fire(self):
-        enemy_range = self.get_enemy_dist()
-        try:
-            if enemy_range <= 50 and enemy_range and not self.handler.global_cooldown:
-                gun = self.handler.copy_gun()
-                gun['image'] = self.img
-                gun['gun_fire_sound'] = self.sound
-                pmod = .2
-                gun['damage_min'] = int(max(gun['damage_min'] * pmod, 1))
-                gun['damage_max'] = int(max(gun['damage_max'] * pmod, 2))
-                for i in range(random.randint(1, 10)):
-                    self.handler.delayed.append([i, partial(LightningMelee, self.master, self.handler, self, gun, self.handler.owner.sprite.x, self.handler.owner.sprite.y)])
-                return True
-            else:
-                self.dash()
-                return False
-        except:
-            return False
-
-    def dash(self):
-        ret = calc_vel_xy(self.handler.owner.sprite.x, self.handler.owner.sprite.y,
-        self.handler.owner.target.sprite.x, self.handler.owner.target.sprite.y, 45)
-        self.handler.owner.controller.move_to(self.handler.owner.target.sprite.x + ret[0],
-            self.handler.owner.target.sprite.y + ret[1], 1)
-
-    def get_enemy_dist(self):
-        if self.handler.owner.target:
-            dist_x = self.handler.owner.sprite.x - self.handler.owner.target.sprite.x
-            dist_y = self.handler.owner.sprite.y - self.handler.owner.target.sprite.y
-            dist = math.hypot(dist_x, dist_y)
-            return dist
-        return False
-
-# Unfinished
-class PSMAIronDome(Skill):
-    def __init__(self, master, level, handler):
-        super(PSMAIronDome, self).__init__(master, level, handler)
-
-# Unfinished
-class PSMAHardCore(Skill):
-    def __init__(self, master, level, handler):
-        super(PSMAHardCore, self).__init__(master, level, handler)
-
-# Unfinished
-class PSMAPerseverate(Skill):
-    def __init__(self, master, level, handler):
-        super(PSMAPerseverate, self).__init__(master, level, handler)
-
-# Unfinished
-class PSMABurst(Skill):
-    def __init__(self, master, level, handler):
-        super(PSMABurst, self).__init__(master, level, handler)
-
-# Unfinished
-class PSMATriggerDiscipline(Skill):
-    def __init__(self, master, level, handler):
-        super(PSMATriggerDiscipline, self).__init__(master, level, handler)
-
-# Unfinished
-class PSMASalvo(Skill):
-    def __init__(self, master, level, handler):
-        super(PSMASalvo, self).__init__(master, level, handler)
-
-# Unfinished
-class PSPOBolt(Skill):
-    def __init__(self, master, level, handler):
-        super(PSPOBolt, self).__init__(master, level, handler)
+        super(LDZap, self).__init__(master, level, handler)
         self.gun_fire_sound = load_sound('lightning_bolt.wav')
 
     def fire(self):
@@ -392,61 +244,9 @@ class PSPOBolt(Skill):
         return False
 
 # Unfinished
-class PSPOAnvilCrawler(Skill):
+class LDSpark(Skill):
     def __init__(self, master, level, handler):
-        super(PSPOAnvilCrawler, self).__init__(master, level, handler)
-        self.gun_fire_sound = load_sound('lightning_bolt.wav')
-
-    def fire(self):
-        hits = 0
-        for e in self.handler.owner.enemies:
-            if hits >= 3:
-                break
-            self.handler.owner.target = e
-            dist = self.get_enemy_dist()
-            if dist <= (self.handler.core.plasma / self.handler.core.plasma_max * 200) + 50:
-                hits += 1
-                self.handler.core.lightning_counter = 3
-                self.handler.core.create_lightning(
-                    self.handler.owner.sprite.x,
-                    self.handler.owner.sprite.y,
-                    dist,
-                    [self.handler.owner.target.sprite.x, self.handler.owner.target.sprite.y]
-                )
-
-                # self.handler.core.bullets -= 1
-                gun = self.handler.copy_gun()
-                gun['accuracy'] += 100
-                gun['gun_fire_sound'] = self.gun_fire_sound
-                pmod = self.handler.core.plasma / self.handler.core.plasma_max * .5
-                gun['gun_fire_sound'] = self.gun_fire_sound
-                gun['damage_min'] = int(max(gun['damage_min'] * pmod, 1))
-                gun['damage_max'] = int(max(gun['damage_max'] * pmod, 2))
-                Gunshot(self.master, self.handler, self, dict.copy(gun), self.handler.owner.target.sprite.x, self.handler.owner.target.sprite.y)
-
-        if hits > 0:
-            self.handler.core.plasma = 0
-            return True
-        return False
-
-    def get_enemy_dist(self):
-        if self.handler.owner.target:
-            dist_x = self.handler.owner.sprite.x - self.handler.owner.target.sprite.x
-            dist_y = self.handler.owner.sprite.y - self.handler.owner.target.sprite.y
-            dist = math.hypot(dist_x, dist_y)
-            return dist
-        return False
-
-
-# Unfinished
-class PSPOBangForYourBuck(Skill):
-    def __init__(self, master, level, handler):
-        super(PSPOBangForYourBuck, self).__init__(master, level, handler)
-
-# Unfinished
-class PSPOSpark(Skill):
-    def __init__(self, master, level, handler):
-        super(PSPOSpark, self).__init__(master, level, handler)
+        super(LDSpark, self).__init__(master, level, handler)
         self.gun_fire_sound = load_sound('lightning_bolt.wav')
 
     def fire(self):
@@ -494,11 +294,40 @@ class PSPOSpark(Skill):
             return dist
         return False
 
+# Unfinished
+class LDChainLightning(Skill):
+    def __init__(self, master, level, handler):
+        super(LDChainLightning, self).__init__(master, level, handler)
 
 # Unfinished
-class PSPOZipZap(Skill):
+class LDBallLightning(Skill):
     def __init__(self, master, level, handler):
-        super(PSPOZipZap, self).__init__(master, level, handler)
+        super(LDBallLightning, self).__init__(master, level, handler)
+
+# Unfinished
+class LDRepurpose(Skill):
+    def __init__(self, master, level, handler):
+        super(LDRepurpose, self).__init__(master, level, handler)
+        self.sound = load_sound('repurpose.wav')
+
+    def fire(self):
+        if self.handler.owner.stats.shield and self.handler.core.plasma != self.handler.core.plasma_max:
+            play_sound(self.sound)
+            self.master.spriteeffect.teleport(self.handler.owner.sprite.x, self.handler.owner.sprite.y)
+            self.handler.core.plasma += 50 * (self.handler.owner.stats.shield / self.handler.owner.stats.shield_max)
+            self.handler.owner.stats.update_health(self.handler.owner.stats.shield)
+            return True
+        return False
+
+# Unfinished
+class LDEmpower(Skill):
+    def __init__(self, master, level, handler):
+        super(LDEmpower, self).__init__(master, level, handler)
+
+# Unfinished
+class LDZipZap(Skill):
+    def __init__(self, master, level, handler):
+        super(LDZipZap, self).__init__(master, level, handler)
         self.img = load_image('strike.png')
         self.sound = load_sound('slash.wav')
 
@@ -536,77 +365,34 @@ class PSPOZipZap(Skill):
             return dist
         return False
 
-# Unfinished
-class PSPOPlasmaBall(Skill):
-    def __init__(self, master, level, handler):
-        super(PSPOPlasmaBall, self).__init__(master, level, handler)
-        self.image = load_image('plasma_ball.png')
-        self.damage_mod = 1 + .05 * self.level
-
-    def fire(self):
-        if self.handler.core.plasma >= 20:
-            self.handler.core.plasma -= 20
-            gun = self.handler.copy_gun()
-            gun['damage_min'] = int(gun['damage_min'] * self.damage_mod)
-            gun['damage_max'] = int(gun['damage_max'] * self.damage_mod)
-            # gun['velocity'] = 30
-            gun['image'] = self.image
-
-            PlayerGunshot(self.master, self.handler, self, dict.copy(gun), self.handler.owner.sprite.x, self.handler.owner.sprite.y)
-            return True
-        return False
 
 # Unfinished
-class PSPOTracer(Skill):
+class LDEnergize(Skill):
     def __init__(self, master, level, handler):
-        super(PSPOTracer, self).__init__(master, level, handler)
+        super(LDEnergize, self).__init__(master, level, handler)
 
 # Unfinished
-class PSPOBlotOutTheSun(Skill):
+class LDRedirect(Skill):
     def __init__(self, master, level, handler):
-        super(PSPOBlotOutTheSun, self).__init__(master, level, handler)
+        super(LDRedirect, self).__init__(master, level, handler)
 
 # Unfinished
-class PSPOConked(Skill):
+class LDRepulse(Skill):
     def __init__(self, master, level, handler):
-        super(PSPOConked, self).__init__(master, level, handler)
-
-# Unfinished
-class PSPOBallLightning(Skill):
-    def __init__(self, master, level, handler):
-        super(PSPOBallLightning, self).__init__(master, level, handler)
-
+        super(LDRepulse, self).__init__(master, level, handler)
 
 plasmaslinger_skillset = {
     'core': PlasmaCore,
-    '1': PSPDRepurpose,
-    '2': PSPDFlakJacket,
-    '3': PSPDTwentyMileMarch,
-    '4': PSPDLockAndLoad,
-    '5': PSPDFletcher,
-    '6': PSPDSpotAndShot,
-    '7': PSPDIronRations,
-    '8': PSPDStrafe,
-    '9': PSPDCannibalize,
-    '10': PSPDBarrage,
-    '11': PSMABang,
-    '12': PSMAShieldSplitter,
-    '13': PSMALightSword,
-    '14': PSMALightSpeed,
-    '15': PSMAIronDome,
-    '16': PSMAHardCore,
-    '17': PSMAPerseverate,
-    '18': PSMABurst,
-    '19': PSMATriggerDiscipline,
-    '20': PSMASalvo,
-    '21': PSPOBolt,
-    '22': PSPOAnvilCrawler,
-    '23': PSPOBangForYourBuck,
-    '24': PSPOSpark,
-    '25': PSPOZipZap,
-    '26': PSPOPlasmaBall,
-    '27': PSPOTracer,
-    '28': PSPOBlotOutTheSun,
-    '29': PSPOConked,
-    '30': PSPOBallLightning,
+    '1': LDBreaker,
+    '2': LDCurrent,
+    '3': LDZap,
+    '4': LDSpark,
+    '5': LDChainLightning,
+    '6': LDBallLightning,
+    '7': LDRepurpose,
+    '8': LDEmpower,
+    '9': LDZipZap,
+    '10': LDEnergize,
+    '11': LDRedirect,
+    '12': LDRepulse,
 }
